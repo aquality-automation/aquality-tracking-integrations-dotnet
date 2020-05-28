2939a4ea3f0a7b93ad4d45fad376cae5c1961320
Pavel Anihimovsky <pavel.anihimovsky@gmail.com>
2020-05-28 19:08:09 +0300

    Add docs to HttpClient

--- AqualityTracking.Integrations/AqualityTracking.Integrations.Core/IHttpClient.cs
+++ AqualityTracking.Integrations/AqualityTracking.Integrations.Core/IHttpClient.cs
@@ -5,10 +5,30 @@ namespace AqualityTracking.Integrations.Core
 {
     public interface IHttpClient
     {
+        /// <summary>
+        /// Sends GET request to specified uri.
+        /// </summary>
+        /// <typeparam name="TResponse">Type of the response data.</typeparam>
+        /// <param name="requestUri">Request URI.</param>
+        /// <returns>Response data.</returns>
         TResponse SendGET<TResponse>(Uri requestUri);
 
+        /// <summary>
+        /// Sends POST request to specified uri.
+        /// </summary>
+        /// <typeparam name="TModel">Type of the response/request body data.</typeparam>
+        /// <param name="requestUri">Request URI.</param>
+        /// <param name="body">Request body which converts to string.</param>
+        /// <returns>Response data.</returns>
         TModel SendPOST<TModel>(Uri requestUri, TModel body);
 
+        /// <summary>
+        /// Sends POST request to specified uri.
+        /// </summary>
+        /// <typeparam name="TResponse">Type of the response data.</typeparam>
+        /// <param name="requestUri">Request URI.</param>
+        /// <param name="content">Request entity.</param>
+        /// <returns>Response data.</returns>
         TResponse SendPOST<TResponse>(Uri requestUri, HttpContent content);
     }
 }
