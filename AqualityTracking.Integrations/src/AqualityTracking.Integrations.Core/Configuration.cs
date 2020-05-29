9b292be2be9e5ec99a53c035eae9f00a656baad3
Pavel Anihimovsky <pavel.anihimovsky@gmail.com>
2020-05-29 13:07:16 +0300

    Implement Configuration

--- AqualityTracking.Integrations/src/AqualityTracking.Integrations.Core/Configuration.cs
+++ AqualityTracking.Integrations/src/AqualityTracking.Integrations.Core/Configuration.cs
@@ -1,31 +1,95 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
-using System.Dynamic;
-using System.Text;
+﻿using AqualityTracking.Integrations.Core.Utilities;
+using Newtonsoft.Json.Linq;
+using System.ComponentModel;
 
 namespace AqualityTracking.Integrations.Core
 {
     public class Configuration
     {
-        public bool Enabled { get; set; }
+        private bool enabled;
+        private string host;
+        private string token;
+        private int projectId;
+        private string executor;
+        private string suiteName;
+        private string buildName;
+        private string environment;
+        private string ciBuild;
+        private bool debug;
 
-        public string Host { get; set; }
+        public bool Enabled
+        {
+            get { return GetValueOrDefault("aquality.enabled", enabled); } 
+            set { enabled = value; }
+        }
 
-        public string Token { get; set; }
+        public string Host
+        {
+            get { return GetValueOrDefault("aquality.host", host); }
+            set { host = value; }
+        }
 
-        public int ProjectId { get; set; }
+        public string Token
+        {
+            get { return GetValueOrDefault("aquality.token", token); }
+            set { token = value; }
+        }
 
-        public string Executor { get; set; }
+        public int ProjectId
+        {
+            get { return GetValueOrDefault("aquality.projectId", projectId); }
+            set { projectId = value; }
+        }
 
-        public string SuiteName { get; set; }
+        public string Executor
+        {
+            get { return GetValueOrDefault("aquality.executor", executor); }
+            set { executor = value; }
+        }
 
-        public string BuildName { get; set; }
+        public string SuiteName
+        {
+            get { return GetValueOrDefault("aquality.suiteName", suiteName); }
+            set { suiteName = value; }
+        }
 
-        public string Environment { get; set; }
+        public string BuildName
+        {
+            get { return GetValueOrDefault("aquality.buildName", buildName); }
+            set { buildName = value; }
+        }
 
-        public string CiBuild { get; set; }
+        public string Environment
+        {
+            get { return GetValueOrDefault("aquality.environment", environment); }
+            set { environment = value; }
+        }
 
-        public bool Debug { get; set; }
+        public string CiBuild
+        {
+            get { return GetValueOrDefault("aquality.ciBuild", ciBuild); }
+            set { ciBuild = value; }
+        }
+
+        public bool Debug
+        {
+            get { return GetValueOrDefault("aquality.debug", debug); }
+            set { debug = value; }
+        }
+
+        private T GetValueOrDefault<T>(string key, T defaultValue)
+        {
+            var envValue = EnvironmentReader.GetVariable(key);
+            if (envValue != null)
+            {
+                return (T) TypeDescriptor.GetConverter(typeof(T)).ConvertFrom(envValue);
+            }
+            return defaultValue;
+        }
+
+        public static Configuration ParseFromJson(string json)
+        {
+            return JObject.Parse(json)?.ToObject<Configuration>();
+        }
     }
 }
---
+++ AqualityTracking.Integrations/src/AqualityTracking.Integrations.Core/Utilities/FileReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace AqualityTracking.Integrations.Core.Utilities
+{
+    public static class FileReader
+    {
+        private static readonly string ResourceFolder = "Resources";
+
+        public static string ReadFromResources(string fileName)
+        {
+            var filePath = GetResourceFilePath(fileName);
+            return File.ReadAllText(filePath);
+        }
+
+        private static string GetResourceFilePath(string fileName)
+        {
+            return Path.Combine(AppContext.BaseDirectory, ResourceFolder, fileName);
+        }
+    }
+}
