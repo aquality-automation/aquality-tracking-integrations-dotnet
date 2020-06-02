c3b8a504089b4e7f3e435186677e8f6cddc0d4b0
Pavel Anihimovsky <pavel.anihimovsky@gmail.com>
2020-06-02 15:56:40 +0300

    Fix build warning

--- AqualityTracking.Integrations/src/AqualityTracking.Integrations.Core/AqualityException.cs
+++ AqualityTracking.Integrations/src/AqualityTracking.Integrations.Core/AqualityException.cs
@@ -1,11 +1,24 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace AqualityTracking.Integrations.Core
 {
     [Serializable]
     public class AqualityException : Exception
     {
+        public AqualityException()
+        {
+        }
+
         public AqualityException(string message) : base(message)
+        {
+        }
+
+        public AqualityException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected AqualityException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
     }
